faaec72ba88f98747303b0f419d2d99d239d210e
jillianguerra <guerrajilliant@gmail.com>
2023-05-06 14:46:50 -0400

    sets win conditions. sets best 3/5 wins.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const COLORS = {
     '0': 'white',
     '1': 'pink',
-    '-1': 'lightGreen',
+    '-1': 'lightgreen',
 }
 const LETTERS = {
     '0': '',
@@ -21,13 +21,18 @@ const score = {
 let turn
 let board
 let winner
+let roundWinner
 /*----- cached elements  -----*/
 const messageEl = document.querySelector('h2')
-const playAgainBtn = document.querySelector('button')
+const playAgainBtn = document.querySelector('#play')
+const resetBoardBtn = document.querySelector('#reset')
 const cellEls = [...document.querySelectorAll('#board > div')]
 const scoreEl = document.querySelector('#score')
+const modalEl = document.querySelector('#modal')
+const modalDivEl = document.querySelector('#modal > div')
 /*----- event listeners -----*/
 playAgainBtn.addEventListener('click', init)
+resetBoardBtn.addEventListener('click', resetScore)
 document.querySelector('#board').addEventListener('click', handleDrop)
 /*----- functions -----*/
 init()
@@ -93,21 +98,70 @@ function handleDrop(evt) {
     // make sure you can't select the same cell more than once
     // make sure you can't select in between the cells
     // alter the array to be 1 or -1
-const nodeIdx = cellEls.indexOf(evt.target)
-if (nodeIdx === -1 || winner) {
-    return
-} else if (board[nodeIdx] !== 0) {
-    return
+    const nodeIdx = cellEls.indexOf(evt.target)
+    if (nodeIdx === -1 || winner) {
+        return
+    } else if (board[nodeIdx] !== 0) {
+        return
+    }
+    else {
+        board[nodeIdx] = turn
+        winner = getWinner(nodeIdx)
+        turn *= -1
+        render()
+    }
 }
-else {
-    board[nodeIdx] = turn
-    console.log(nodeIdx)
+function getWinner(idx) {
+    if (3 === Math.abs(board[0] + board[1] + board[2]) ||
+        3 === Math.abs(board[3] + board[4] + board[5]) ||
+        3 === Math.abs(board[6] + board[7] + board[8]) ||
+        3 === Math.abs(board[0] + board[3] + board[6]) ||
+        3 === Math.abs(board[1] + board[4] + board[7]) ||
+        3 === Math.abs(board[2] + board[5] + board[8]) ||
+        3 === Math.abs(board[0] + board[4] + board[8]) ||
+        3 === Math.abs(board[2] + board[4] + board[6])) { 
+            const player = board[idx]
+            score[player] ++
+            getRoundWinner(player)
+            return player
+        } else if (board.includes(0) === false) {
+            console.log('Tie')
+        return 'T'
+    }
 }
-console.log(board)
-winner = getWinner(nodeIdx)
-turn *= -1
-render()
+function getRoundWinner(player) {
+    if (3 === score[player]) {
+        modalEl.classList.toggle('open')
+        modalDivEl.innerHTML = `<span style='color: ${COLORS[player]}'>${NAMES[player].toUpperCase()}</span> WINS!`
+        playAgainBtn.style.visibility = 'hidden'
+    }
 }
-function getWinner() {
+function resetScore() {
+    score['1'] = 0
+    score['-1'] = 0
+    init()
+    modalEl.classList.toggle('open')
+}
+// do a loop to go through the array and find if there are any zeros in the array. 
+// If there aren't any 0, then it's a tie.
 
-}
+// Winning board states:
+//horizontal
+// [1, 1, 1, 0, 0, 0, 0, 0, 0]
+// arr[0] arr[1] arr[2]
+// [0, 0, 0, 1, 1, 1, 0, 0, 0]
+// arr[3] arr[4] arr[5]
+// [0, 0, 0, 0, 0, 0, 1, 1, 1]
+// arr[6] arr[7] arr[8]
+//vertical
+// [1, 0, 0, 1, 0, 0, 1, 0, 0]
+// arr [0] arr [3] arr[6]
+// [0, 1, 0, 0, 1, 0, 0, 1, 0]
+// arr[1] arr[4] arr[7]
+// [0, 0, 1, 0, 0, 1, 0, 0, 1]
+// arr[2] arr[5] arr[8]
+//diagonal
+// [1, 0, 0, 0, 1, 0, 0, 0, 1]
+// arr[0] arr[4] arr[8]
+// [0, 0, 1, 0, 1, 0, 1, 0, 0]
+// arr[2] arr[4] arr[6]
